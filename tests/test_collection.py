9c9b0168791c1405438311420eeaa814d56edacf
Edoardo Vacchi <evacchi@users.noreply.github.com>
2022-07-22 16:52:20 +0200

    restore test collection (failing)

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -6,21 +6,21 @@ from ansible_events.collection import (
 )
 
 
-# def test_find_collection():
-#     location = find_collection("community.general")
-#     assert location is not None
-#
-#
-# def test_find_collection_eda():
-#     location = find_collection("benthomasson.eda")
-#     assert location is not None
-#
-#
-# def test_find_source():
-#     location = find_source(*split_collection_name("benthomasson.eda.range"))
-#     assert location is not None
-#
-#
-# def test_load_rules():
-#     rules = load_rules(*split_collection_name("benthomasson.eda.hello_events"))
-#     assert rules is not None
+def test_find_collection():
+    location = find_collection("community.general")
+    assert location is not None
+
+
+def test_find_collection_eda():
+    location = find_collection("benthomasson.eda")
+    assert location is not None
+
+
+def test_find_source():
+    location = find_source(*split_collection_name("benthomasson.eda.range"))
+    assert location is not None
+
+
+def test_load_rules():
+    rules = load_rules(*split_collection_name("benthomasson.eda.hello_events"))
+    assert rules is not None
